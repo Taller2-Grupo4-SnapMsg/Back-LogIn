069c1ce2fc9a441d1a052d5c51c2c7ae86e2b7eb
Merok23 <alejocalizblanco@gmail.com>
2023-10-13 20:49:50 -0300

    Se termino de migrar a okteto, bye render!

--- run.sh
+++
@@ -1,4 +0,0 @@
-# This script is for seeing the endpoints connected to the data base without going through render
-# Use with caution.
-docker build -t loginback .
-docker run -p 8000:8000 loginback
