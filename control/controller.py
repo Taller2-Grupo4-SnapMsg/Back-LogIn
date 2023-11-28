a7defb92ec498a78fb6339d2a4f015d1d9f533fc
Merok23 <alejocalizblanco@gmail.com>
2023-11-28 13:55:22 -0300

    Se agrego una config de okteto, y en el controller. vamo a ver que sale.

--- control/controller.py
+++ control/controller.py
@@ -6,10 +6,17 @@ This is the controller layer of the REST API for the user's backend.
 # Para permitir pegarle a la API desde localhost:
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-from ddtrace.runtime import RuntimeMetrics
+from ddtrace import tracer, patch, config
 from control.routers import followers, admins, users, users_put
 
-RuntimeMetrics.enable()
+patch(fastapi=True)
+
+config.fastapi["request_span_name"] = "http.request"
+
+tracer.configure(hostname="datadog_stats", port=8126)
+tracer.trace("users-api", service="back-users")
+tracer.set_tags({"env": "dev"})
+tracer.set_tags({"service": "back-users"})
 
 app = FastAPI(
     title="User's API", description="This is the API for the user's microservice."
