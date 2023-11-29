ec5d595df2bdc2b5cb853cd2912fb5b39934e317
Merok23 <alejocalizblanco@gmail.com>
2023-11-29 15:01:14 -0300

    De baja datadog, uptrace is my new best friend.

--- control/controller.py
+++ control/controller.py
@@ -3,20 +3,31 @@
 """
 This is the controller layer of the REST API for the user's backend.
 """
+import os
+import uptrace
+from opentelemetry import trace
+
 # Para permitir pegarle a la API desde localhost:
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-from ddtrace import tracer, patch, config
 from control.routers import followers, admins, users, users_put
 
-patch(fastapi=True)
+uptrace.configure_opentelemetry(
+    dsn=os.environ.get("UPTRACE_DSN"),
+    service_name="myservice",
+    service_version="1.0.0",
+    deployment_environment="production",
+)
 
-config.fastapi["request_span_name"] = "http.request"
+tracer = trace.get_tracer("app_or_package_name", "1.0.0")
 
-tracer.configure(hostname="datadog_stats", port=8126)
-tracer.trace("users-api", service="back-users")
-tracer.set_tags({"env": "dev"})
-tracer.set_tags({"service": "back-users"})
+with tracer.start_as_current_span("foo") as main:
+    with tracer.start_as_current_span("bar"):
+        print("Hello world from OpenTelemetry Python!")
+
+    print("trace:", uptrace.trace_url(main))
+
+uptrace.shutdown()
 
 app = FastAPI(
     title="User's API", description="This is the API for the user's microservice."
